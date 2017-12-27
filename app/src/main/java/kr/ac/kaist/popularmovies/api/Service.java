7537c0be26129638e4c2ebfe65da203b48439bae
heeinso <heeinso@snu.ac.kr>
2017-12-27 10:36:11 +0900

    Create a service class

---
+++ app/src/main/java/kr/ac/kaist/popularmovies/api/Service.java
@@ -0,0 +1,14 @@
+package kr.ac.kaist.popularmovies.api;
+
+import kr.ac.kaist.popularmovies.model.MoviesResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Service {
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+}
