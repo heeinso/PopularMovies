ba69a2b3c59419bce7644a775806101581018b03
heeinso <heeinso@snu.ac.kr>
2017-12-29 14:43:07 +0900

    Create a trailer response model class

---
+++ app/src/main/java/kr/ac/kaist/popularmovies/model/TrailerResponse.java
@@ -0,0 +1,25 @@
+package kr.ac.kaist.popularmovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TrailerResponse {
+    @SerializedName("id")
+    private int id_trailer;
+
+    @SerializedName("results")
+    private List<Trailer> results;
+
+    public int getId_trailer() {
+        return id_trailer;
+    }
+
+    public void setId_trailer(int id_trailer) {
+        this.id_trailer = id_trailer;
+    }
+
+    public List<Trailer> getResults() {
+        return results;
+    }
+}
