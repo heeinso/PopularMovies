ff95c1735cd4245fdbd371f21107ccd50485c281
heeinso <heeinso@snu.ac.kr>
2017-12-31 16:15:45 +0900

    Implement a favorite button function on detail activity

--- app/src/main/java/kr/ac/kaist/popularmovies/DetailActivity.java
+++ app/src/main/java/kr/ac/kaist/popularmovies/DetailActivity.java
@@ -1,9 +1,12 @@
 package kr.ac.kaist.popularmovies;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.github.ivbaranov.mfb.MaterialFavoriteButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -73,6 +77,27 @@ public class DetailActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "No API data", Toast.LENGTH_SHORT).show();
         }
+
+        MaterialFavoriteButton materialFavoriteButton = (MaterialFavoriteButton) findViewById(R.id.favorite_button);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        materialFavoriteButton.setOnFavoriteChangeListener(new MaterialFavoriteButton.OnFavoriteChangeListener() {
+            @Override
+            public void onFavoriteChanged(MaterialFavoriteButton buttonView, boolean favorite) {
+                if (favorite) {
+                    SharedPreferences.Editor editor = getSharedPreferences(".DetailActivity", MODE_PRIVATE).edit();
+                    editor.putBoolean("Favorite Added", true);
+                    editor.commit();
+                    Snackbar.make(buttonView, "Added to Favorite", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    SharedPreferences.Editor editor = getSharedPreferences(".DetailActivity", MODE_PRIVATE).edit();
+                    editor.putBoolean("Favorite Removed", false);
+                    editor.commit();
+                    Snackbar.make(buttonView, "Removed from Favorite", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         initViews();
     }
